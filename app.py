cccb0bdcc3935aa72f6c163bc0c09541ef974c1b
zjowy <162414479+zjowy@users.noreply.github.com>
2024-03-07 11:31:55 +0000

    Page with style

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "Hello Jory"
-if __name__=="__main__":
-  app.run(host="0.0.0.0",port=True)
+  return render_template('home.html')
+
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=True)
