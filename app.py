b0c730154e119e74a665fabf799c2bd530ebf6c2
zjowy <162414479+zjowy@users.noreply.github.com>
2024-03-21 11:03:24 +0000

    receive massage and save to db

--- app.py
+++ app.py
@@ -1,13 +1,36 @@
-from flask import Flask , render_template
-from database import load_projects_from_db
+from flask import Flask, render_template, request
+from database import load_projects_from_db, load_project_from_db,add_massage_to_db
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  projects= load_projects_from_db()
-  return render_template('home.html',myskills=projects,myname='Jury Alsultan')
+  projects = load_projects_from_db()
+  return render_template('home.html',
+                         myskills=projects,
+                         myname='Jury Alsultan')
 
+
+@app.route("/project/<id>")
+def show_project(id):
+  project = load_project_from_db(id)
+  return render_template('projectdetails.html', project=project)
+
+
+@app.route("/contactus")
+def contactpage():
+  return render_template('contactus.html')
+
+
+@app.route("/massage", methods=['post'])
+def massagepage():
+  data = request.form
+
+  add_massage_to_db(data)
+  return render_template('massage_submitted.html', massage=data)
+
+  
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=True)
--- database.py
+++ database.py
@@ -1,13 +1,31 @@
 from sqlalchemy import create_engine, text
-import os 
-db_connection_string =os.environ['DB_CONNECTION_STRING']
+import os
+
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(db_connection_string)
+
 
 def load_projects_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from myprojects"))
-    myprojects=[]
+    myprojects = []
     for row in result.all():
       myprojects.append(dict(row._mapping))
     return myprojects
+
+
+def load_project_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"select * from myprojects where id = {id}"))
+    rows = result.all()
+    return dict(rows[0]._mapping)
+
+def add_massage_to_db(data):
+  with engine.connect() as conn:
+    sql=text( f"INSERT INTO massages (Full_Name,email,massage) VALUES (\'{data['Full Name']}\',\'{data['email']}\',\'{data['massage']}\')"
+    )
+
+
+    conn.execute(sql)
+    conn.commit()
